5dce3009cf3383078cf1ddd1a39781925639a49c
blackbird806 <blackbirdcry806@gmail.com>
2024-10-29 11:38:45 +0100

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv)
 {
 	std::print("hello {}", "world");
 	
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	if (!SDL_Init(SDL_INIT_VIDEO))
 	{
 		std::print("failed to init sdl");
 		return -1;
@@ -26,6 +26,35 @@ int main(int argc, char** argv)
 
 	SDL_Texture* renderTarget = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, gameSizeX, gameSizeY);
 	SDL_SetTextureScaleMode(renderTarget, SDL_SCALEMODE_NEAREST);
+
+	bool endGame = false;
+
+	while (!endGame)
+	{
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_EVENT_QUIT:
+				endGame = true;
+				break;
+
+			default:
+				break;
+			}
+		}
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyTexture(renderTarget);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 
     return 0;
 }
